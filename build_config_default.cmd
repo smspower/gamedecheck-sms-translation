42c4e769e001fdcb5a7543300f2e8bf93defb413
Omar <omarcornut@gmail.com>
2020-03-08 16:26:03 +0100

    Added simple Readme and editable build config

---
+++ build_config_default.cmd
@@ -0,0 +1,22 @@
+@rem ###########################################################################
+@rem Default Configuration file, DO NOT edit, if you want to provide your own
+@rem paths, please copy this file into "build_config.cmd", which will be ignored
+@rem by git
+@rem ###########################################################################
+
+@rem ###########################################################################
+@rem # BMP2TILE 0.50+
+@set CONFIG_BMP2TILE="C:\sms\bmp2tile\BMP2Tile.exe"
+
+@rem ###########################################################################
+@rem # WLA-DX 2020+
+@set CONFIG_WLAZ80="C:\sms\wla-dx\binaries\wla-z80.exe"
+@set CONFIG_WLALINK="C:\sms\wla-dx\binaries\wlalink.exe"
+
+@rem ###########################################################################
+@rem # PYTHON 3.x+
+@set CONFIG_PYTHON="python.exe"
+
+@rem ###########################################################################
+@rem # EMULATORS
+@set CONFIG_MEKA="C:\sms\meka\mekaw.exe"
