086eb82a761bd9a37399c7ce59d8dfab84dd074f
Maxim <maxim@smspower.org>
2020-03-11 19:35:42 +0000

    Defaults to suit the GH actions build

--- build_config_default.cmd
+++ build_config_default.cmd
@@ -6,12 +6,12 @@
 
 @rem ###########################################################################
 @rem # BMP2TILE 0.50+
-@set CONFIG_BMP2TILE="C:\sms\bmp2tile\BMP2Tile.exe"
+@set CONFIG_BMP2TILE="%dp0\tools\BMP2Tile.exe"
 
 @rem ###########################################################################
 @rem # WLA-DX 2020+
-@set CONFIG_WLAZ80="C:\sms\wla-dx\binaries\wla-z80.exe"
-@set CONFIG_WLALINK="C:\sms\wla-dx\binaries\wlalink.exe"
+@set CONFIG_WLAZ80="%dp0\tools\wla-z80.exe"
+@set CONFIG_WLALINK="%dp0\tools\wlalink.exe"
 
 @rem ###########################################################################
 @rem # PYTHON 3.x+
